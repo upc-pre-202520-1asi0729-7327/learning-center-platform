7f504d9553decfd4f78291a3b076afa25f13d2f1
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-29 16:25:52 -0500

    feat: add auditable base classes for jpa entities.

---
+++ src/main/java/com/acme/center/platform/shared/domain/model/aggregates/AuditableAbstractAggregateRoot.java
@@ -0,0 +1,32 @@
+package com.acme.center.platform.shared.domain.model.aggregates;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.domain.AbstractAggregateRoot;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class AuditableAbstractAggregateRoot<T extends AbstractAggregateRoot<T>> extends AbstractAggregateRoot<T> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    public void addDomainEvent(Object event) {
+        this.registerEvent(event);
+    }
+}
---
+++ src/main/java/com/acme/center/platform/shared/domain/model/entities/AuditableModel.java
@@ -0,0 +1,28 @@
+package com.acme.center.platform.shared.domain.model.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class AuditableModel {
+    @Id
+    @Getter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Getter
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Getter
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+
+}
