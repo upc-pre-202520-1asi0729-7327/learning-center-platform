711f86942b64f3462cd879d418d9c75055ab1d47
Angel Velasquez <pcisavel@upc.edu.pe>
2025-11-10 20:35:52 -0500

    fix(hashing): update documentation for bcrypt-hashing-service interface usage.

--- src/main/java/com/acme/center/platform/iam/infrastructure/hashing/bcrypt/BCryptHashingService.java
+++ src/main/java/com/acme/center/platform/iam/infrastructure/hashing/bcrypt/BCryptHashingService.java
@@ -6,7 +6,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 /**
  * This interface is a marker interface for the BCrypt hashing service.
  * It extends the {@link HashingService} and {@link PasswordEncoder} interfaces.
- * This interface is used to inject the BCrypt hashing service in the {@link com.acme.learning.platform.iam.infrastructure.hashing.bcrypt.services.HashingServiceImpl} class.
+ * This interface is used to inject the BCrypt hashing service in the application.
  */
 public interface BCryptHashingService extends HashingService, PasswordEncoder {
 }
