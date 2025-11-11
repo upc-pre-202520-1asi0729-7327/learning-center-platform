396100f68d76030035a190edd304b4a3b784094e
Angel Velasquez <pcisavel@upc.edu.pe>
2025-11-10 20:27:00 -0500

    feat(iam): add resources and assemblers for user authentication and role management.

---
+++ src/main/java/com/acme/center/platform/iam/interfaces/rest/transform/SignUpCommandFromResourceAssembler.java
@@ -0,0 +1,22 @@
+package com.acme.center.platform.iam.interfaces.rest.transform;
+
+import com.acme.center.platform.iam.domain.model.commands.SignUpCommand;
+import com.acme.center.platform.iam.domain.model.entities.Role;
+import com.acme.center.platform.iam.interfaces.rest.resources.SignUpResource;
+
+import java.util.ArrayList;
+
+/**
+ * Assembler to convert a SignUpResource to a SignUpCommand.
+ */
+public class SignUpCommandFromResourceAssembler {
+    /**
+     * Converts a SignUpResource to a SignUpCommand.
+     * @param resource the SignUpResource to convert
+     * @return the SignUpCommand
+     */
+    public static SignUpCommand toCommandFromResource(SignUpResource resource) {
+        var roles = resource.roles() != null ? resource.roles().stream().map(name -> Role.toRoleFromName(name)).toList() : new ArrayList<Role>();
+        return new SignUpCommand(resource.username(), resource.password(), roles);
+    }
+}
