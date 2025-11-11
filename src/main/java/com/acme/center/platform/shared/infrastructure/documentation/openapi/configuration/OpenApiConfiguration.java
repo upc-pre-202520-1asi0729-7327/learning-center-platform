8ece03e2e870035efa8b553113caa9e5ec3bc1c4
Angel Velasquez <pcisavel@upc.edu.pe>
2025-11-10 20:24:53 -0500

    feat(open-api): add security scheme for jwt authentication in open-api configuration.

--- src/main/java/com/acme/center/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/acme/center/platform/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -1,10 +1,13 @@
 package com.acme.center.platform.shared.infrastructure.documentation.openapi.configuration;
 
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -46,8 +49,8 @@ public class OpenApiConfiguration {
     @Bean
     public OpenAPI learningPlatformOpenApi() {
 
-
         // General configuration
+
         var openApi = new OpenAPI();
         openApi
                 .info(new Info()
@@ -60,6 +63,21 @@ public class OpenApiConfiguration {
                         .description("ACME Learning Platform wiki Documentation")
                         .url("https://acme-learning-platform.wiki.github.io/docs"));
 
+        // Add a security scheme
+
+        final String securitySchemeName = "bearerAuth";
+
+        openApi.addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+
+        // Return the OpenAPI configuration object with all the settings
 
         return openApi;
     }
